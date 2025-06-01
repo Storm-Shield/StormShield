dcb90fc5ecc09d1a7098a8b306710e364f16a5f9
Vitor <vitramosf@gmail.com>
2025-05-31 22:00:07 -0300

    Adicionando link para tecnologias

--- src/script/loading.js
+++ src/script/loading.js
@@ -36,8 +36,5 @@ document.addEventListener("DOMContentLoaded", function() {
 
 });
 
-window.addEventListener('load', () => {
-    document.body.classList.remove('loading');
-    document.body.classList.add('loaded');
-});
+
 
