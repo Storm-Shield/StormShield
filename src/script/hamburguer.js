b14578bd44aaa4f566cfbd19816cfbf4004f79c4
CarlosSMariano <sanchesmarianocarlos@gmail.com>
2025-06-02 18:52:33 -0300

    Adicionando smooth scroll em js (para deixar mais chique, mas dá pra fazer apenas com css)

--- src/script/hamburguer.js
+++ src/script/hamburguer.js
@@ -4,8 +4,8 @@ function menuView() {
 
     body.style.overflowY = 'hidden';
     console.log('evento chamado')
-    let menu = document.querySelector('.menu-mobile');
-    let icon = document.getElementById('menu-hamburger')
+    let menu = document.querySelector('.mobile');
+    let icon = document.querySelector('.menu-hamburger > span')
 
     if (menu.classList.contains('open')) {
         menu.classList.remove('open');
@@ -14,15 +14,15 @@ function menuView() {
     } else {
         menu.classList.add('open');
         body.style.overflowY = 'hidden';
-        icon.className = 'line-md--menu-to-close-transition';
+        icon.className = "line-md--menu-to-close-transition";
     }
 
 }
 
 document.addEventListener('click', function (event) {
 
-    let menu = document.querySelector('.menu-mobile');
-    let icon = document.getElementById('menu-hamburger');
+    let menu = document.querySelector('.mobile');
+    let icon = document.querySelector('.menu-hamburger > span');
     let menuMobile = document.querySelector('.button-menu-mobile')
     if (!menu.contains(event.target) && !menuMobile.contains(event.target)) {
         if (menu.classList.contains('open')) {
--- src/script/script.js
+++ src/script/script.js
@@ -1,6 +1,4 @@
 // SCRIPT CARROSSEL
-
-
 let index = 0;
 const slides = document.querySelectorAll('.slide');
 
@@ -10,3 +8,31 @@ setInterval(() => {
     slides[index].classList.add('active');
 }, 3000); // troca a cada 3 segundos
 
+//SCROLL
+const links = document.querySelectorAll('.menu a[href^="#"]');
+
+function getDistanciaTopo(element){
+    const id = element.getAttribute("href");
+    return document.querySelector(id).offsetTop;
+}
+
+function scrollWindow(distanciaTopo){
+
+      window.scroll({
+        top : distanciaTopo,
+        behavior: "smooth"
+    })
+  
+  
+
+}
+
+function scrollToSection(event){
+    event.preventDefault();
+    const distanciaTopo = getDistanciaTopo(event.target) - 90;
+    scrollWindow(distanciaTopo);
+}
+
+links.forEach((link) =>{
+link.addEventListener("click", scrollToSection);
+});
